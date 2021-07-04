1ca6f7f367eadf8eb1bd51843e7a488690d902ee
hiroto <hiroto.t.923@gmail.com>
2021-07-04 21:46:05 +0900

    [変更点] ・SyouhizeiActivity,TanikansanActivity,NengouActivity,MairuActivity,RiekiActivityの追加

---
+++ app/src/main/java/com/example/a210704_calculator03/TanikansanActivity.kt
@@ -0,0 +1,11 @@
+package com.example.a210704_calculator03
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class TanikansanActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_tanikansan)
+    }
+}
