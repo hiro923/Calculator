88ddbf64390060bac06637bf8e432ad637980590
matsumura <kaito.03528@gmail.com>
2021-07-04 17:21:34 +0900

    通貨換算のファイルの追加

---
+++ app/src/main/java/com/example/a210704_calculator03/tuukaActivity.kt
@@ -0,0 +1,12 @@
+package com.example.a210704_calculator03
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class tuukaActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_tuuka)
+    }
+}
+
