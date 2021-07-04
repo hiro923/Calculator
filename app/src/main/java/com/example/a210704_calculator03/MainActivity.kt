c0dfba0543c882778c938e0a723e5891507b1be4
hiroto <hiroto.t.923@gmail.com>
2021-07-04 16:23:11 +0900

    Initial commit

---
+++ app/src/main/java/com/example/a210704_calculator03/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.example.a210704_calculator03
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.a210704_calculator03.databinding.ActivityMainBinding
+
+private lateinit var binding: ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val view = binding.root
+        setContentView(view)
+    }
+
+    binding.
+}
